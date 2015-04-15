f3b2d67446201986dd19187511afd159ef4af0c2
Willy Chen <terepic@gmail.com>
2015-04-15 16:14:22 -0400

    Removed comments relating to own linked list implementation

--- LRUcache.cpp
+++ LRUcache.cpp
@@ -43,51 +43,8 @@ int set(string key, string data){
 	return 1;
 }
 
-// void LRUcache::add(Node node){
-// 	node->next = NULL;
-// 	node->previous = NULL;
-
-// 	if(head == NULL){
-// 		head = node;
-// 		end = node;
-// 		return;
-// 	}
-
-// 	head->previous = node;
-// 	node->next = head;
-// 	head = node;
-// }
-
-// void LRUcache::remove(Node node){
-// 	if(node == NULL || head == NULL)
-// 		return;
-// 	if(head == end && head == node){ //only node
-// 		head = NULL;
-// 		end = NULL;
-// 		return;
-// 	}
-// 	if(node == head){ //remove from head
-// 		head->next->previous = NULL;
-// 		head = head->next;
-// 		return;
-// 	}
-// 	if(node == end){//remove from end
-// 		end->previous->next = NULL;
-// 		end = end->previous;
-// 		return;
-// 	}
-
-// 	node->previous->next = node->next;
-// 	node->next->previous = node->previous;
-// }
 
 void LRUcache::moveToFirst(Node node){
 	queue.remove(node);
 	queue.push_front(node);
-	//remove(node);
-	//add(node);
 }
-
-// void LRUcache::removeLast(){
-// 	remove(end);
-// }
--- LRUcache.hpp
+++ LRUcache.hpp
@@ -7,8 +7,6 @@ class Node{
 public:
 	string data; //data = response - http header
 	string key; //key = url
-	//Node previous;
-	//Node next;
 };
 
 class LRUcache{
@@ -16,17 +14,12 @@ class LRUcache{
 	int usedMemory;
 	map<int, Node> data;
 	list<Node> queue;
-	//Node head;
-	//Node tail;
 
 public:
 	LRUcache(int size);
-	string get(string key);
+	string get(string key); //Returns data or NULL if not found
 	int set(string key, string data);
 
 private:
-	//void add(Node node); //Append node as head
-	//void remove(Node node);
 	void moveToFirst(Node node);
-	//void removeLast();
 };
