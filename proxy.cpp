19b68e7b2cf44b4c1068a33a09e8d9dfd129b36d
Willy Chen <terepic@gmail.com>
2015-04-18 23:34:01 -0400

    bind

--- proxy.cpp
+++ proxy.cpp
@@ -28,14 +28,6 @@ sockaddr_in sAdr(string str){
 int Proxy::listenForBrowser(){
 	//Do something to listen to the port and receive the http requests from the browser
 	char buf[8190];
-<<<<<<< HEAD
-	struct sockaddr_in my_addr=sAdr(port);
-	int sock, conn_sock;
-	socklen_t len;
-	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		perror("Create socket error:");
-		return -1;
-=======
 	struct sockaddr_in sin=sAdr(port);
 	int serv_sock, new_sock;
 	socklen_t len;
@@ -43,7 +35,6 @@ int Proxy::listenForBrowser(){
 	if((serv_sock = ::socket(PF_INET, SOCK_STREAM,0)) <0){
 		perror("Create socket error");
 		exit(1);
->>>>>>> a14fb97141a509b3449ebe0e41bf0c99217a0938
 	}
 
 	if((::bind(serv_sock, (struct sockaddr *)&sin, sizeof(sin))) < 0){
@@ -54,14 +45,9 @@ int Proxy::listenForBrowser(){
 
 	/*Wait for connection loop*/
 	while(1){
-<<<<<<< HEAD
-		len = sizeof(my_addr);
-		if((conn_sock = ::accept(sock, (struct sockaddr *)&my_addr, &len))<0){
-=======
 
 		fprintf(stderr, "Server is listening\n");
 		if((new_sock = ::accept(serv_sock, (struct sockaddr *)&sin, &len))<0){
->>>>>>> a14fb97141a509b3449ebe0e41bf0c99217a0938
 			perror("Connection failed");
 			exit(1);
 		}
